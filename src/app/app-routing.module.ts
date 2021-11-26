0a9ac1011906630541d0c4605e07b97d8534856e
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-11-26 12:30:05 -0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
    {
-      path: '', redirectTo: 'home', pathMatch: 'full'
-   },
-   {
       path: 'home', component: HomeComponent
    },
    {
